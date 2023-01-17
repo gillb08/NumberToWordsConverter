4fba1cf542f1d1f1e11f213377fb1be651916142
Victor Gilbert Chijioke <111375035+gillb08@users.noreply.github.com>
2023-01-17 01:32:11 +0100

    Add files via upload

--- NumberToWordConverter/NumberToWordConverter/NumToWord.cs
+++ NumberToWordConverter/NumberToWordConverter/NumToWord.cs
@@ -1,7 +1,67 @@
-﻿namespace NumberToWordConverter
-{
-    public class NumbersToWords
-    {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
+namespace NumberToWordConverter
+{
+    public class NumberToWord
+    {
+        private static string[] ones =
+        {
+            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+        };
+
+        private static string[] tens =
+        {
+            "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+        };
+
+        public static string ConvertNumberToWords(int number)
+        {
+            if (number == 0)
+                return "zero";
+
+            if (number < 0)
+                return "minus " + ConvertNumberToWords(Math.Abs(number));
+
+            string words = "";
+
+            if ((number / 1000000) > 0)
+            {
+                words += ConvertNumberToWords(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += ConvertNumberToWords(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += ConvertNumberToWords(number / 100) + " hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                if (number < 20)
+                    words += ones[number];
+                else
+                {
+                    words += tens[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + ones[number % 10];
+                }
+            }
+
+            return words;
+        }
     }
 }
